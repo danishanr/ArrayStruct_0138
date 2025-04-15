6a4e5008a355cb58f09c40422478cdb6375efe96
danishanr <danishaanr@gmail.com>
2025-04-15 19:51:35 +0700

    Penambahan kode Getline agar bisa menulis nama panjang

---
+++ structGetline.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+   
+
+int main(){
+    //membuat object struct mahasiswa
+    mahasiswa mhs;
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cin.ignore();
+    cout << "Masukkan NAMA = ";
+    getline (cin, mhs.nama);
+    cout << "Masukkan ALAMAT DESA = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan ALAMAT KOTA = ";
+    cin >> mhs.alamat.kota;
+
+    cout << endl;
+    cout << "Data Mahasiswa " << endl;
+
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "NAMA = " << mhs.nama << endl;
+    cout << "ALAMAT DESA = " << mhs.alamat.desa << endl;
+    cout << "ALAMAT DESA = " << mhs.alamat.kota << endl; 
+
+    
+}
